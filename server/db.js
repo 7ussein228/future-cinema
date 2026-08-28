29218da05bbea78912700bf43732b49c6195b6fb
Hussein <7ussein228@gmail.com>
2026-08-28 16:25:01 +0300

    rename coupon RORO100 to RORO175

--- server/db.js
+++ server/db.js
@@ -30,7 +30,7 @@ export const DEFAULT_CONCESSIONS = [
 
 export const DEFAULT_COUPONS = [
   { code: 'FUTURE20', discount: 0.2, type: 'percent', active: true, expiresAt: null, minAmount: 0 },
-  { code: 'RORO100', type: 'seats_free', maxSeats: 2, active: true, expiresAt: null, minAmount: 0 }
+  { code: 'RORO175', type: 'seats_free', maxSeats: 2, active: true, expiresAt: null, minAmount: 0 }
 ]
 
 let db = null
